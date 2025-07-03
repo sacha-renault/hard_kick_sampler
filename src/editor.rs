aab6aacd0926e1dfd5b7dc266152d7df10f2d4e3
sacha-renault <sacha35340@gmail.com>
2025-07-03 10:42:16 +0200

    fix: ctx.input placement

--- src/editor.rs
+++ src/editor.rs
@@ -17,18 +17,18 @@ pub fn create_editor(
         params.clone(),
         |_ctx, _params| {},
         move |ctx, _setter, _state| {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                // Enable drag and drop
-                ctx.input(|i| {
-                    if !i.raw.dropped_files.is_empty() {
-                        for file in &i.raw.dropped_files {
-                            if let Some(path) = &file.path {
-                                async_executor.execute_background(TaskIn::LoadFile(0, path.clone()));
-                            }
+            // Enable drag and drop
+            ctx.input(|i| {
+                if !i.raw.dropped_files.is_empty() {
+                    for file in &i.raw.dropped_files {
+                        if let Some(path) = &file.path {
+                            async_executor.execute_background(TaskIn::LoadFile(0, path.clone()));
                         }
                     }
-                });
+                }
+            });
 
+            egui::CentralPanel::default().show(ctx, |ui| {
                 if ui.button("Click").clicked() {
                     async_executor.execute_background(TaskIn::LoadFile(5, PathBuf::from(r"C:\Program Files\Image-Line\FL Studio 21\Data\Patches\Packs\Custom pack\OPS\OPS - Euphoric Hardstyle Kick Expansion (Vol. 1)\Kick Build Folder\Crunches\OPS_ECHKE1_CRUNCH_3_F.wav")));
                 }
