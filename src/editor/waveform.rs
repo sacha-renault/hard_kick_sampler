421189bd92894b8228def54c6dbc0494390be393
Sacha RENAULT <sacha35340@gmail.com>
2025-07-09 11:21:52 +0200

    feat: add step by for waveform

--- src/editor/waveform.rs
+++ src/editor/waveform.rs
@@ -12,12 +12,13 @@ pub fn render_waveform_stereo(
 ) {
     let skip = trim_start * sample_rate * num_channels as f32 + channel_index as f32;
     let add_silent = (delay_start * sample_rate) as usize;
+    let step_by = get_step_by_value(ui.available_width(), data.len() as f32, num_channels as f32);
 
     let silent_data = vec![[0.0, 0.0], [add_silent as f64, 0.0]];
     let plot_data = data
         .iter()
         .skip(skip as usize)
-        .step_by(num_channels)
+        .step_by(step_by)
         .enumerate()
         .map(|(i, &y)| [(add_silent + i) as f64, y as f64]);
     Plot::new(format!("{}Plot", channel_index))
@@ -43,4 +44,27 @@ pub fn render_waveform_stereo(
                 .color(Color32::GREEN),
             );
         });
+}
+
+/// Calculate the increment to avoid too may data
+///
+/// # Arguments
+///
+/// * `width` - width of the viewport in pxl
+/// * `num_data` - number of data in the plot
+/// * `num_channels` - number of channel in the interleaved data
+fn get_step_by_value(width: f32, num_data: f32, num_channels: f32) -> usize {
+    if width <= 0. {
+        return num_channels as usize;
+    }
+
+    let per_channel_num = num_data / num_channels;
+
+    if per_channel_num <= width {
+        return num_channels as usize;
+    }
+
+    let samples_to_skip = per_channel_num / width;
+    let density = 1.; // might change later
+    (samples_to_skip / density).max(1.) as usize * num_channels as usize
 }
